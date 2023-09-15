5d1cd4d0c0cfdd2b72acda9c312c2807b7dc102b
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-09-15 22:43:22 +0200

    update basic config

--- app.config.ts
+++ app.config.ts
@@ -1,16 +1,11 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'Dystopia Documentation',
+    description: 'Documentation for Dystopia open source repositories',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
+      twitter: 'dystcz',
+      github: 'dystcz'
     },
     github: {
       dir: '.starters/default/content',
@@ -29,7 +24,7 @@ export default defineAppConfig({
       fluid: true
     },
     header: {
-      logo: true,
+      logo: false,
       showLinkIcon: true,
       exclude: [],
       fluid: true
